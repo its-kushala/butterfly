a49065d33175f1a0ed93b4515cc673c846e57d81
Kushala C L <103378341+its-kushala@users.noreply.github.com>
2024-07-15 13:27:05 +0530

    butterfly uploaded

--- src/stars.java
+++ src/stars.java
@@ -1,12 +1,13 @@
 public class stars {
     public static void main(String[] args) {
         int n=5;
-
+//upper part
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=i; j++) {
                 System.out.print("*");
             }
             int spaces = 2 * (n-i);
+            //space part
             for(int j=1; j<=spaces; j++) {
                 System.out.print(" ");
             }
@@ -15,10 +16,12 @@ public class stars {
             }
             System.out.println();
         }
+        //lower part
         for(int i=n; i>=1; i--){
             for(int j=1; j<=i; j++) {
                 System.out.print("*");
             }
+            //space part
             int spaces=2*(n-i);
             for(int j=1; j<=spaces; j++){
                 System.out.print(" ");
