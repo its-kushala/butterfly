e616bcb283599609b63caff122085440a2517d60
Kushala C L <103378341+its-kushala@users.noreply.github.com>
2024-07-15 14:51:28 +0530

    butterfly is created

--- src/stars.java
+++ src/stars.java
@@ -1,7 +1,7 @@
 public class stars {
     public static void main(String[] args) {
         int n = 5;
-//upper part
+        //upper part
         for (int i=1; i<=n; i++) {
             for(int j=1; j<=i; j++) {
                 System.out.print("*");
