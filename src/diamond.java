812264974fe2527f796b7a9cf14d75c16c4424f3
Kushala C L <103378341+its-kushala@users.noreply.github.com>
2024-07-15 14:49:08 +0530

    diamond is formed

--- src/diamond.java
+++ src/diamond.java
@@ -1,27 +1,26 @@
-public class pyramid {
+public class diamond {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 4;
         for (int i = 1; i <= n; i++) {
             //space
             for (int j = 1; j <= (n - i); j++) {
                 System.out.print(" ");
             }
             //numbers
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*" + " ");
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
-        for (int i = n; i <= 1; i--) {
+        for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= (n - i); j++) {
                 System.out.print(" ");
             }
             //numbers
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*" + " ");
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
     }
 }
-
